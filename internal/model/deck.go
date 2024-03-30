6cf7db299b428c71ff5d661dd728fe0d2fd6376e
axseem <tr1xn.gg@gmail.com>
2024-03-30 14:45:25 +0100

    feat: struct validation via tags

--- internal/model/deck.go
+++ internal/model/deck.go
@@ -6,16 +6,16 @@ import (
 )
 
 type Deck struct {
-	ID        string
-	Title     string
-	Cards     [][2]string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        string      `json:"id"`
+	Title     string      `json:"title"`
+	Cards     [][2]string `json:"cards"`
+	CreatedAt time.Time   `json:"createdAt"`
+	UpdatedAt time.Time   `json:"updatedAt"`
 }
 
 type DeckCreateParams struct {
-	Title string
-	Cards [][2]string
+	Title string      `json:"title" validate:"required,gt=0,lte=256"`
+	Cards [][2]string `json:"cards" validate:"required,gt=0,lte=4096,dive,required,dive,required,gt=0,lte=1024"`
 }
 
 type DeckRepo interface {
@@ -24,6 +24,4 @@ type DeckRepo interface {
 	Create(ctx context.Context, arg DeckCreateParams) (*Deck, error)
 	Update(ctx context.Context, id string, arg DeckCreateParams) (*Deck, error)
 	Delete(ctx context.Context, id string) error
-	ValidateCreateParams(arg DeckCreateParams) error
-	ValidateUpdateParams(ctx context.Context, arg DeckUpdateParams) error
 }
