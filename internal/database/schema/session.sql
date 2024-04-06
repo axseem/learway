4056228f80b3ab2fb123bdec238c89b0ed90910c
axseem <tr1xn.gg@gmail.com>
2024-04-06 14:20:09 +0200

    refactor: use blob for ip

--- internal/database/schema/session.sql
+++ internal/database/schema/session.sql
@@ -2,7 +2,7 @@ CREATE TABLE session (
     id TEXT PRIMARY KEY,
     user_id TEXT NOT NULL,
     fingerprint BLOB NOT NULL,
-    ip INTEGER NOT NULL,
+    ip BLOB NOT NULL,
     expires_at DATETIME NOT NULL,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES user (id)
